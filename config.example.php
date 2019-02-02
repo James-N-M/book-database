086ba8c27019338847fcbce75b3b620f5d874199
James Moore <jamesm0025@gmail.com>
2019-02-02 14:14:56 -0500

    add example config file

---
+++ config.example.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'database' => [
+        'name' => 'books',
+        'username' => 'root',
+        'password' => 'secret',
+        'connection' => 'mysql:host=127.0.0.1',
+        'options' => [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
+        ]
+    ]
+];
