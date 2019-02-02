1280eaf404c860fb599ee8da162403c079c4e332
James Moore <jamesm0025@gmail.com>
2019-02-02 14:14:56 -0500

    add example config file

---
+++ config.example.php
@@ -0,0 +1,10 @@
+tabase' => [
+        'name' => 'books',
+        'username' => 'root',
+        'password' => 'secret',
+        'connection' => 'mysql:host=127.0.0.1',
+        'options' => [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
+        ]
+    ]
+];
