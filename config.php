98b8d8f6291548a30d7b2c0250ecb24cfba45c44
James Moore <jamesm0025@gmail.com>
2019-02-02 20:14:32 -0500

    echo to index

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 return [
     'database' => [
-        'name' => 'books',
-        'username' => 'root',
-        'password' => 'secret',
-        'connection' => 'mysql:host=127.0.0.1',
+        'name' => 'heroku_2f80ffa7a7ef55c',
+        'username' => 'b91c71858054c5',
+        'password' => 'd4a433e0',
+        'connection' => 'us-cdbr-iron-east-03.cleardb.net',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
         ]
