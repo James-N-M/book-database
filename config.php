5b723835a216960cfc4bab4943e7a1faed474701
James Moore <jamesm0025@gmail.com>
2019-02-03 10:27:44 -0500

    changed config back to default cause router isnt working on heroku build

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 return [
     'database' => [
-        'name' => 'zxzg0o7rys0zrq5k',
-        'username' => 'fhk1o73upjtqhlu6',
-        'password' => 'drso4vsjj6tt2nz4',
-        'connection' => 'd13xat1hwxt21t45.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        'name' => 'books',
+        'username' => 'root',
+        'password' => 'secret',
+        'connection' => '127.0.0.1',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ]
