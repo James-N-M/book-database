e3dd53a935721767439a0434e9102507f5f754fa
James Moore <jamesm0025@gmail.com>
2019-02-02 19:55:05 -0500

    added new online database creds

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'database' => [
+        'name' => 'ct0zp7a0domlcu2r',
+        'username' => 'buic2stc8a5xj40p',
+        'password' => 'wminuf42e7i975bq',
+        'connection' => 'd13xat1hwxt21t45.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        'options' => [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
+        ]
+    ]
+];
